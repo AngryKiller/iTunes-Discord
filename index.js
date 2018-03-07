05305c75148c214c782abee94e98dd99142f931b
Steve Labus <steve.labus@uphoria.org>
2018-03-07 01:09:56 +0100

    A lot of progress!
    iTunes-Discord now sends requests in a loop using KofiLoop! It means that the informations are updated and sent dynamically. I still have some problems with time remaining tho.

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const client = require('discord-rich-presence')('420530637485637644');
 const iTunes = require('itunes-bridge');
+const kofiloop = require('kofiloop');
 
+console.log('iTunes-Discord started! Wait 15 seconds for the first RPC request to be sent...');
+kofiloop.startLoop(function(){
+    var playerState = iTunes.getPlayerState();
+    if(playerState == 'playing') {
+        var details = " 🎧  Playing " + iTunes.getCurrentTrackName() + " by " + iTunes.getCurrentTrackArtist();
+        var state = "💿  " + iTunes.getCurrentTrackAlbum();
+        var duration = iTunes.getCurrentTrackDuration();
+        const endTimestamp = Date.now() + iTunes.getCurrentTrackRemainingSeconds();
+        client.updatePresence({
+            state: state,
+            details: details,
+            largeImageKey: 'itunes_large',
+            smallImageKey: 'playing',
+            instance: true,
+        });
+        console.log("iTunes is playing, informations sent: " + details + " " + state );
+    }else if(playerState == 'paused'){
+        var details = "⏸  Paused: " + iTunes.getCurrentTrackName() + " by " + iTunes.getCurrentTrackArtist();
+        var state = "💿  " + iTunes.getCurrentTrackAlbum();
+        client.updatePresence({
+            state: state,
+            details: details,
+            largeImageKey: 'itunes_large',
+            smallImageKey: 'paused',
+            instance: true,
+        });
+        console.log("iTunes is paused, informations sent: " + details + " " + state );
+    }else if(playerState == 'stopped'){
+        client.updatePresence({});
+        console.log("iTunes is not playing! do u kno da wae to close a Discord RPC connection? ");
+    }
+}, 15000);
 
-var state = iTunes.getCurrentTrackName() + " - " + iTunes.getCurrentTrackArtist();
-var details = iTunes.getCurrentTrackAlbum();
-var duration = iTunes.getCurrentTrackDuration();
-client.updatePresence({
-    state: state,
-    details: details,
-    largeImageKey: 'itunes_large',
-    instance: true,
-});
-
-console.log(state);
-console.log(details);
-console.log(duration);
+// wen eta gui? SON.
