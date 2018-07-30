bc456259cfeea71298313a099fb665fc9a5674e5
Steve Labus <angrykiller@icloud.com>
2018-07-30 22:31:39 +0200

    Redesign of the preferences window

--- assets/js/dialog1.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  const electron = require('electron');
-
-  const currWindow = electron.remote.getCurrentWindow();
-
-  document.querySelector(".btn-system").addEventListener("click", function() {
-    currWindow.close();
-  });
-})();
--- functions/windows.js
+++ functions/windows.js
@@ -7,11 +7,11 @@ const views = path.join(__dirname, '../views');
 let preferencesWindow;
 
 exports.showPreferences = function() {
-        preferencesWindow = new BrowserWindow({width: 400, height: 300, fullscreenable: false});
+        preferencesWindow = new BrowserWindow({width: 520, height: 450, minWidth: 520, minHeight: 450, fullscreenable: false, titleBarStyle: "hiddenInset"});
         preferencesWindow.loadURL(url.format({
             pathname: path.join(views, 'preferences.html'),
             protocol: 'file:',
             slashes: true
         }));
-    preferencesWindow.webContents.openDevTools();
+    //preferencesWindow.webContents.openDevTools();
 };
