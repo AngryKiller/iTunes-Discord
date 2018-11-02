5023e2217391eeb37b70d017d35a4e859280590d
Steve Labus <angrykiller@icloud.com>
2018-11-02 01:13:26 +0100

    App isn’t longer quitting when the preferences window is closed

--- utils/windows.js
+++ utils/windows.js
@@ -7,7 +7,7 @@ const Store = require('electron-store');
 const store = new Store();
 var exports = module.exports = {};
 const views = path.join(__dirname, '../views');
-
+let willQuitApp = false;
 let preferencesWindow;
 
 lang.list(function(languages){
@@ -26,11 +26,33 @@ ejse.data('line1', store.get('line-1'));
 ejse.data('line2', store.get('line-2'));
 
 exports.showPreferences = function() {
-        preferencesWindow = new BrowserWindow({width: 520, height: 671, minWidth: 520, minHeight: 671, fullscreenable: false, titleBarStyle: "hiddenInset", vibrancy: "medium-light", transparent: true});
-        preferencesWindow.loadURL(url.format({
-            pathname: path.join(views, 'preferences.ejs'),
-            protocol: 'file:',
-            slashes: true
-        }));
-        preferencesWindow.setMenu(null);
-};
+    preferencesWindow = new BrowserWindow({
+        width: 520,
+        height: 671,
+        minWidth: 520,
+        minHeight: 671,
+        fullscreenable: false,
+        titleBarStyle: "hiddenInset",
+        vibrancy: "medium-light",
+        transparent: true
+    });
+    preferencesWindow.loadURL(url.format({
+        pathname: path.join(views, 'preferences.ejs'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    preferencesWindow.setMenu(null);
+
+    preferencesWindow.on('close', (e) => {
+        if (willQuitApp) {
+            /* the user tried to quit the app */
+            preferencesWindow = null;
+        } else {
+            /* the user only tried to close the window */
+            e.preventDefault();
+            preferencesWindow.hide();
+        }
+    });
+}
+
+app.on('before-quit', () => willQuitApp = true);
